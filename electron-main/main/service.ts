cf243a9a44baec06e909c7faa6cb29e8835d3b87
Roki Ran <ranxiaojing7096@gmail.com>
2022-12-08 21:51:45 +0800

    ws 通了

--- electron-main/main/service.ts
+++ electron-main/main/service.ts
@@ -1,30 +1,56 @@
 const initWs = () => {
   // 初始化express服务
-  const express = require('express');
-  const app = express();
-  const port = 4000;
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-    // 允许跨域
-    res.header('Access-Control-Allow-Origin', '*');
+  // const express = require('express');
+  // const app = express();
+  // const port = 80;
+  // app.get('/', (req, res) => {
+  //   res.send('Hello World!');
+  //   // 允许跨域
+  //   res.header('Access-Control-Allow-Origin', '*');
+  // });
+  // app.get('socket.io', (req, res) => {
+  //   res.send('Hello World!');
+  //   // 允许跨域
+  //   res.header('Access-Control-Allow-Origin', '*');
+  // });
+  // app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+  // });
+  // // 初始化scokett.io服务
+  // const server = require('http').createServer(app);
+  // const io = require('socket.io')(server, {
+  //   cors: true,
+  // });
+  // io.on('connection', (socket) => {
+  //   console.log('a user connected');
+  //   socket.on('disconnect', () => {
+  //     console.log('user disconnected');
+  //   });
+  //   socket.on('chat message', (msg) => {
+  //     console.log;
+  //     io.emit('chat message', msg);
+  //   });
+  // });
+  const app = require('express')();
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http, {
+    cors: true,
+  });
+  app.get('/', function (req, res) {
+    res.send('<h1>Welcome Realtime Server</h1>');
   });
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    // 初始化scokett.io服务
-    const server = require('http').createServer(app);
-    const io = require('socket.io')(server, {
-      cors: true,
+  http.listen(80, function () {
+    console.log('listening on *:80');
+  });
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
     });
-    io.on('connection', (socket) => {
-      console.log('a user connected');
-      socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
-      socket.on('chat message', (msg) => {
-        console.log;
-        io.emit('chat message', msg);
-      });
+    socket.on('chat message', (msg) => {
+      console.log;
+      io.emit('chat message', msg);
     });
   });
 };
